3bfed9aa93a524d06259f07460b8a4694b35059f
EC2 Default User <ec2-user@ip-172-31-1-15.ap-northeast-1.compute.internal>
2022-09-26 13:21:03 +0000

    destroy機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,8 @@ class BooksController < ApplicationController
   def view
   end
 
+
+
   def index
     @books = Book.all
     @book = Book.new
@@ -13,7 +15,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -21,6 +23,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   def book_params
